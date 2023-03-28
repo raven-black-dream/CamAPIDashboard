5549512cded52dec9bd8712bc9c43d2f75a0b499
evan <evan.harley21@gmail.com>
2023-03-28 18:07:03 +0000

    Debugging Continues

--- debug_app.py
+++ debug_app.py
@@ -20,7 +20,7 @@ debugpy.listen(5678)
 print("Waiting for debugger attach")
 debugpy.wait_for_client()
 debugpy.breakpoint()
-print('break on this line')5
+print('break on this line')
 markdown.empty()
 
 app.main()
